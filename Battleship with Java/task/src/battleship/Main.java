e2ad147c9702bc1b9291dccfa7fa0419467f667b
svd2vjb <vandenlangenbergh.s@gmail.com>
2024-10-30 10:14:56 +0100

    feat: calculate length of ship

--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -9,28 +9,37 @@ public class Main {
         Field field = new Field();
         System.out.println(field);
 
+
+        // Get input and keep asking for input until valid input is given
         boolean noValidInputYet = true;
         while (noValidInputYet) {
-         // Get input
-            String coordinates = KeyboardUtil.getInput("Enter the coördinates of the ship:");
-            String length = KeyboardUtil.getInput();
-            String parts = KeyboardUtil.getInput();
 
-        // Split in 2 coordinates
+            // Get input
+            String coordinates = KeyboardUtil.getInput("Enter the coördinates of the ship:");
+
+            // Split in 2 coordinates
             String[] splitCoordinates = coordinates.split(" ");
             String firstCoordinate = splitCoordinates[0];
             String secondCoordinate = splitCoordinates[1];
-        // Get number from coordinates
+            // Get number from coordinates
             String firstCoordNumber = firstCoordinate.substring(1);
             String secondCoordNumber = secondCoordinate.substring(1);
 
-        try {
-            System.out.println(field.validateCoordinates(firstCoordinate, secondCoordinate, firstCoordNumber, secondCoordNumber));
-            noValidInputYet = false;
-        } catch (IllegalArgumentException iae) {
-            System.out.println(iae.getMessage());
+            try {
+                System.out.println(field.validateCoordinates(firstCoordinate, secondCoordinate, firstCoordNumber, secondCoordNumber));
+                noValidInputYet = false;
+            } catch (IllegalArgumentException iae) {
+                System.out.println(iae.getMessage());
+        }
+
+        // Calculate and print length of the ship
+            if (firstCoordinate.charAt(0) != secondCoordinate.charAt(0)) {
+                System.out.println("Length:" + (Math.abs((secondCoordinate.charAt(0) - firstCoordinate.charAt(0))) + 1));
+            } else {
+                System.out.println("Length:" + (Math.abs((Integer.parseInt(secondCoordNumber) - Integer.parseInt(firstCoordNumber))) + 1));
         }
     }
 }
 }
+
 
