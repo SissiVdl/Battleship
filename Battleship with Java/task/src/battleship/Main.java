98ed97580ba1bd51d7c1e89c2d47d38efb772a38
svd2vjb <vandenlangenbergh.s@gmail.com>
2024-10-31 13:35:21 +0100

    feat: place all ships on the field (no validation on length yes)

--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -2,6 +2,7 @@ package battleship;
 
 import util.KeyboardUtil;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
@@ -10,43 +11,51 @@ public class Main {
         Field field = new Field();
         System.out.println(field);
 
-        boolean tooCloseToOtherShip = true;
-        while (tooCloseToOtherShip) {
         Ship aircraftCarrier = new Ship(5, "Aircraft Carrier", 'A');
         Ship battleship = new Ship(4, "Battleship", 'B');
         Ship submarine = new Ship(3, "Submarine", 'S');
         Ship cruiser = new Ship(3, "Cruiser", 'C');
         Ship destroyer = new Ship(2, "Destroyer", 'D');
+
+        // Make an array of all the ships
+
         Ship[] allShips = {aircraftCarrier, battleship, submarine, cruiser, destroyer};
 
-            // Get input and keep asking for input until valid input is given
-            boolean inValidCoordinates = true;
-            Coordinates coordinates = null;
+        for (Ship allShip : allShips) {
 
-            while (inValidCoordinates) {
+            boolean tooCloseToOtherShip = true;
+            while (tooCloseToOtherShip) {
 
-                String input = KeyboardUtil.getInput("Enter the coördinates of the ship:");
+                // Get input and keep asking for input until valid input is given
+                boolean inValidCoordinates = true;
+                Coordinates coordinates = null;
 
-                coordinates = new Coordinates(input);
+                while (inValidCoordinates) {
 
+                    String input = KeyboardUtil.getInput("Enter the coördinates of the " + allShip.getName() + " (" + allShip.getSize() + " cells):");
+
+                    coordinates = new Coordinates(input);
+
+                    try {
+                        coordinates.validateCoordinates();
+                        inValidCoordinates = false;
+                    } catch (IllegalArgumentException iae) {
+                        System.out.println(iae.getMessage());
+                    }
+                }
+
+                // Try to place the ship on the field
                 try {
-                    coordinates.validateCoordinates();
-                    inValidCoordinates = false;
+                    field.placeShip(coordinates.getFirstCoordinate(), coordinates.getSecondCoordinate(), coordinates.getFirstCoordNumber(), coordinates.getSecondCoordNumber());
+                    System.out.println(field);
+                    tooCloseToOtherShip = false;
                 } catch (IllegalArgumentException iae) {
                     System.out.println(iae.getMessage());
                 }
             }
-
-            // Try to place the ship on the field
-            try {
-                field.placeShip(coordinates.getFirstCoordinate(), coordinates.getSecondCoordinate(), coordinates.getFirstCoordNumber(), coordinates.getSecondCoordNumber());
-                System.out.println(field);
-                tooCloseToOtherShip = false;
-            } catch (IllegalArgumentException iae) {
-                System.out.println(iae.getMessage());
-            }
         }
     }
 }
+
 
 
